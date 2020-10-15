31c300af3db0d5e88f885bfea5327884e5829df8
”AYANO” <ec2-user@ip-172-31-23-129.us-east-2.compute.internal>
2020-10-15 07:34:41 +0000

    課題提出03-2

--- assignment3.php
+++ assignment3.php
@@ -1,33 +1,35 @@
 <?php
 
 $name = "Ayano";
-
-if ($name = "Ayano") {
+//$name = "Ayano"となっていた部分を修正(異なる名前をいれると「あなたの名前ではありません」表示が出るように。)
+if ($name == "Ayano") {
     echo "私はAyanoです。";
 } else {
-    echo "Ayanoではありません。";
+    echo "あなたの名前ではありません。";
 }
-
+//($i = 0; $i <= 10000; $i++)となっていた部分を修正
+//$total = 0; echo $totalと記述していた部分を一部削除
 $total = 0;
-echo $total;
 
-for ($i =0; $i <=10000; $i++) {
+for ($i = 1; $i <= 10000; $i++) {
     $total += $i;
 }
 echo $total;
 
 $fruits = array("strawberry","banana","peach","melon","apple");
-
-foreach($fruits as $fruits){
-    echo "要素は" . $fruits;
+//forereach($fruits as $fruits)としていた部分を修正
+//echo "要素は" . $fruits; としていた部分を修正
+foreach($fruits as $fruit){
+    echo "要素は" . $fruit;
     echo "\n";
 }
-
+//echo "\n"を追加し改行して結果を表示
 $start = 1;
 $end = 100;
 
 for($i = $start; $i <= $end; $i++) {
     if($i % 5 == 0){
         echo $i;
+        echo "\n";
     }
 }
